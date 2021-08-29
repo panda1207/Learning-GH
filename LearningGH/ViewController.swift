b827c146c10b16fa3a2a82020b152507f191d954
Franky <panda120790@gmail.com>
2021-08-29 12:15:16 +0700

    Add label and function

--- LearningGH/ViewController.swift
+++ LearningGH/ViewController.swift
@@ -16,8 +16,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        sayHello()
     }
-
+    
+    func sayHello() {
+        
+        print("Hello World")
+    }
 
 }
 
